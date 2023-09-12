9dbdaf5d9354c2b0ba4a2f3d64ec6b511df08177
wasiu15 <crownprincewadas@gmail.com>
2023-09-12 02:21:11 +0100

    repositorycontext and contextfactory class were created and I am about to run the first migration

--- Infrastructure/Repositories/RepositoryContext.cs
+++ Infrastructure/Repositories/RepositoryContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Repositories
 {
-    internal class RepositoryContext
+    public class RepositoryContext : DbContext
     {
+        public RepositoryContext(DbContextOptions options): base(options)
+        {
+        }
+
+        public DbSet<Task>? Tasks { get; set; }
     }
 }
---
+++ Presentation/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Presentation.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+            var builder = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseSqlite(configuration.GetConnectionString("sqlConnection"));
+            return new RepositoryContext(builder.Options);
+        }
+
+    }
+}
